5f6c54e11e70bbac453c98dcfcaa8500374d4b9a
Michael Caveney <dylanesque27@gmail.com>
2019-11-18 12:35:19 -0500

    Added initial styling directives

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,17 @@ import SEO from "../components/seo"
 const IndexPage = ({ siteDescription }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>{siteDescription}</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
 
+    <div
+      style={{
+        maxWidth: `300px`,
+        marginBottom: `1.45rem`,
+        textAlign: `center`,
+      }}
+    >
+      <p>Building something beautiful today?</p>
+      <p>This is where you start!</p>
     </div>
-
   </Layout>
 )
 
