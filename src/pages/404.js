dde0346df35ae05bb30504e31f67b996a2312e4f
Michael Caveney <dylanesque27@gmail.com>
2019-11-19 18:01:52 -0500

    Finishing touches

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import vince from '../images/vince.gif'
+import vince from "../images/vince.gif"
+import { Placeholder } from "../components/placeholder-div"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <img src={vince} alt="Vince from Pulp Fiction looking around"></img>
-    <p>It's not here, chief.</p>
+    <Placeholder>
+      <p>404: I don't know where that resource is...</p>
+      <img src={vince} alt="Vince from Pulp Fiction looking around"></img>
+      <p>...but it ain't here, chief.</p>
+    </Placeholder>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,18 @@
 import React from "react"
-import styled from '@emotion/styled';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Placeholder } from "../components/placeholder-div"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
-    <PlaceholderText>
+    <Placeholder>
       <p>Building something beautiful today?</p>
       <p>Start right here!</p>
-    </PlaceholderText>
+    </Placeholder>
   </Layout>
 )
 
 export default IndexPage
-
-const PlaceholderText = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-size: 24px;
-  justify-content: center;
-  align-items: center;
-  margin: 10rem;
-  padding: 20px;
-
-`
